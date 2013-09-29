5b780676c29412b73ff911c5590abfa3a7bb4779
Pavan Reddy <pavankumar.kh@gmail.com>
2013-09-29 01:49:31 -0500

    mapred chaining - confidence of itemsets

--- src/my/ids594/rulemining/RuleMining.java
+++ src/my/ids594/rulemining/RuleMining.java
@@ -2,10 +2,13 @@ package my.ids594.rulemining;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -27,27 +30,39 @@ public class RuleMining {
 		
 		
 		try {
-			JobConf conf = new JobConf(RuleMining.class);
-			conf.setJobName("Association Rule Mining");
+			JobConf conf1 = new JobConf(RuleMining.class);
+			conf1.setJobName("Association Rule Mining - Mapred1");
 			
-			conf.setOutputKeyClass(Text.class);
-		    conf.setOutputValueClass(IntWritable.class);
+			conf1.setOutputKeyClass(Text.class);
+		    conf1.setOutputValueClass(IntWritable.class);
 			
-			conf.setMapperClass(Map.class);
-		    conf.setReducerClass(Reduce.class);
+			conf1.setMapperClass(Map.class);
+		    conf1.setReducerClass(Reduce.class);
 	
-		    conf.setInputFormat(TextInputFormat.class);
-		    conf.setOutputFormat(TextOutputFormat.class);
-		    FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		    conf1.setInputFormat(TextInputFormat.class);
+		    conf1.setOutputFormat(TextOutputFormat.class);
+		    FileInputFormat.setInputPaths(conf1, new Path(args[0]));
+		    FileOutputFormat.setOutputPath(conf1, new Path(args[1]));
 	    
-			JobClient.runJob(conf);
-			
-		} catch (IOException e) {
+		    JobConf conf2 = new JobConf(RuleMining.class);
+			conf2.setJobName("Association Rule Mining - Mapred2");
 
-			e.printStackTrace();
-		}
-		catch (Exception e) {
+			conf2.setOutputKeyClass(Text.class);
+		    conf2.setOutputValueClass(Text.class);
+		    
+		    conf2.setMapperClass(Map2.class);
+		    conf2.setReducerClass(Reduce2.class);
+		    
+		    conf2.setInputFormat(TextInputFormat.class);
+		    conf2.setOutputFormat(TextOutputFormat.class);
+		    FileInputFormat.setInputPaths(conf2, new Path(args[1]));
+		    FileOutputFormat.setOutputPath(conf2, new Path(args[2]));
+		    
+		    
+			JobClient.runJob(conf1);
+			JobClient.runJob(conf2);
+			
+		} catch (Exception e) {
 			
 			e.printStackTrace();
 		}
@@ -65,11 +80,12 @@ public class RuleMining {
 			List<String> itemsets = getItemSets(transaction.split(","));
 			
 			for(String itmset : itemsets){				
-				itemset.set(itmset);
+				itemset.set(itmset.replaceAll(" ", ""));
 				im_output.collect(itemset, new IntWritable(1));
 			}			
 		}
 
+		//getting powersets (exclusing empty set)
 		private List<String> getItemSets(String[] items) {
 			
 			List<String> itemsets = new ArrayList<String>();	
@@ -83,17 +99,17 @@ public class RuleMining {
 			
 			for (int i = 0; i < (1 << n); i++){				
 				
-				String x = "(";  //List<String> newList = new ArrayList<String>(n);
+				List<String> newList = new ArrayList<String>(n);
+				
 				for (int j = 0; j < n; j++){
 	                if ((masks[j] & i) != 0){
 	                	
-	                	x = x + items[j] + ",";  //newList.add(items[j]);
+	                	newList.add(items[j]);
 	                }
 	                
-	                if(j == n-1){
-	                	x = x.substring(0,x.length()-1) + ")";
-	                	if(x != ")")	                	
-	                		itemsets.add(x);
+	                if(j == n-1 && newList.size() > 0){
+              		
+	                	itemsets.add(newList.toString());
 	                }
 				}
 			}
@@ -102,17 +118,126 @@ public class RuleMining {
 		}		
 	}
 	
-	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, Text> {
 
 		@Override
-		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter)
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, Text> output, Reporter reporter)
 				throws IOException {
 			
 			int sum = 0;
-		       	while (values.hasNext()) {
-		       		sum += values.next().get();
-		       }
-		       output.collect(key, new IntWritable(sum));
+		    while (values.hasNext()) {
+		    	sum += values.next().get();
+		    }		    
+		    
+		    output.collect(key, new Text(Integer.toString(sum)));
 		}			
-	}		
+	}
+	
+	public static class Map2 extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+		
+		private Text itemset = new Text();
+		private Text val = new Text();
+		
+		@Override
+		public void map(LongWritable key, Text value, OutputCollector<Text, Text> im_output, Reporter reporter)
+				throws IOException {
+
+			String[] valueSplit = value.toString().split("\\t");
+			
+			itemset.set(valueSplit[0].replaceAll(" ", ""));	//[1,2,3]
+			val.set("0;" + valueSplit[1]); //2
+			
+			//original itemset
+			im_output.collect(itemset, val);
+			
+			String[] items = valueSplit[0].replace("[", "").replace("]", "").split(",");
+			
+			if(items.length > 1){
+			
+				List<String> subitemsets = getItemSets(items);
+				
+				for(String itmset : subitemsets){
+					
+					itemset.set(itmset.replaceAll(" ", ""));
+					val.set(valueSplit[0] + ";" + valueSplit[1]);
+					im_output.collect(itemset, val);
+				}
+			}
+		}
+
+		private List<String> getItemSets(String[] items) {
+			
+			List<String> itemsets = new ArrayList<String>();		
+			int n = items.length;			
+			int[] masks = new int[n];
+			
+			for (int i = 0; i < n; i++)
+	            masks[i] = (1 << i);
+			
+			for (int i = 0; i < (1 << n); i++){				
+				List<String> newList = new ArrayList<String>(n);
+				for (int j = 0; j < n; j++){
+	                if ((masks[j] & i) != 0){
+	                	newList.add(items[j]);
+	                }
+	                
+	                if(j == n-1 && newList.size() == n-1){	              		
+	                	itemsets.add(newList.toString());
+	                }
+				}				
+			} 
+			
+			return itemsets;
+		}
+	}
+	
+	public static class Reduce2 extends MapReduceBase implements Reducer<Text, Text, Text, FloatWritable> {
+				
+		@Override
+		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, FloatWritable> output, Reporter reporter)
+				throws IOException {
+
+			int keyCount = 0;
+			HashMap<String,Integer> hashMap = new HashMap<String,Integer>();
+			
+			String[] keyitems = key.toString().replace("[", "").replace("]", "").split(",");
+				      
+			while (values.hasNext()) {
+
+				String val = values.next().toString();
+				String[] valuesplit = val.split(";");   //[1,2,3];0 OR 0;1
+				
+	       		if(valuesplit[0].equals("0")){	       			
+	       			keyCount = Integer.parseInt(valuesplit[1]);
+	       		}
+	       		else{
+	       			String[] parentItems = valuesplit[0].replace("[", "").replace("]", "").split(",");
+	       			hashMap.put(key + " -> " + getSeparateItem(parentItems,keyitems),  Integer.parseInt(valuesplit[1]));
+	       		}	       		
+			}
+			
+			Iterator iterator = hashMap.keySet().iterator();
+		       
+	        while(iterator.hasNext()){
+	        	String k = iterator.next().toString();
+	        	int v = hashMap.get(k);
+	        	
+	        	output.collect(new Text(k.replace("[", "{").replace("]", "}")), new FloatWritable(v/(float)keyCount));
+	        }
+		}
+
+		private String getSeparateItem(String[] parentItems, String[] keyitems) {
+			
+			String item = null;
+			List<String> items = Arrays.asList(keyitems);
+			
+			for(String s : parentItems){
+				if(!items.contains(s)){
+					item = s;
+					break;
+				}
+			}				
+			return item;
+		}			
+	}
 }
